356abbae8c42bc614caeba00e52a71b1141b6f34
david20033 <daviddimitrov203@gmail.com>
2024-12-10 16:34:00 +0200

    Add addMovie view

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCinema.Data;
+
+namespace MyCinema.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly MyCinemaDBContext _context;
+        public AdminController(MyCinemaDBContext context) 
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+    }
+}
